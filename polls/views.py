7db99a536add277b0ab3ff67bb26a667576fdb93
Yousef Majeed <uaso0of056@gmail.com>
2020-12-05 14:54:06 +0300

    fix detail view

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from .models import Choice, Question
 import datetime
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -20,19 +21,23 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     queryset = Question.objects.filter(pub_date__lte=datetime.datetime.now())
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
